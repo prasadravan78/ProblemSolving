6988b981fd35f39add3b1b554dc038809649bd84
Prasad Ravan <prasadcravan@gmail.com>
2023-02-16 22:05:08 +0530

    Day47 Maximum Depth of Binary Array

---
+++ 047MaximumDepthBinaryArray/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _047MaximumDepthBinaryArray
+{
+    /*
+    Given the root of a binary tree, return its maximum depth.
+    A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+    Example 1:
+    Input: root = [3,9,20,null,null,15,7]
+    Output: 3
+
+    Example 2:
+    Input: root = [1,null,2]
+    Output: 2 
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxDepth(new TreeNode(1)));
+            Console.ReadLine();
+        }
+
+        public static int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
