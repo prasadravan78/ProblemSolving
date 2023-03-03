f8101f4042aecb5ca1bdf3198c5af0b73c5fda6f
Prasad Ravan <prasadcravan@gmail.com>
2023-03-03 18:31:20 +0530

    Day62 First Occurrence In string.

---
+++ 062FirstOccurrenceInString/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _062FirstOccurrenceInString
+{
+    /*
+    Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+    Example 1:
+    Input: haystack = "sadbutsad", needle = "sad"
+    Output: 0
+    Explanation: "sad" occurs at index 0 and 6.
+    The first occurrence is at index 0, so we return 0.
+
+    Example 2:
+    Input: haystack = "leetcode", needle = "leeto"
+    Output: -1
+    Explanation: "leeto" did not occur in "leetcode", so we return -1. 
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StrStr("sadbutsad", "sad"));
+            Console.ReadLine();
+        }
+
+        public static int StrStr(string haystack, string needle)
+        {
+            if (needle.Length == 0)
+            {
+                return 0;
+            }
+
+            if (haystack.Contains(needle))
+            {
+                for (int i = 0; i < haystack.Length; i++)
+                {
+                    if (needle.Equals(haystack.Substring(i, needle.Length)))
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
