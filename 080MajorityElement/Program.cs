9d65e453adfdb135a702f8ee0d9bc671572a7701
Prasad Ravan <prasadcravan@gmail.com>
2024-05-16 20:12:22 +0530

    Majority Element in sorted array

---
+++ 080MajorityElement/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _080MajorityElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MajorityElement(new int[] { 3,2,3}));
+            Console.ReadKey();
+        }
+
+        public static int MajorityElement(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (var num in nums)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    dict[num] = dict[num] + 1;
+                }
+                else
+                {
+                    dict.Add(num, 1);
+                }
+            }
+
+            return dict.MaxBy(k=> k.Value).Key;
+        }
+    }
+}
